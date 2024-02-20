6372e6efeaab69cc58805aad93367188177e2c15
edvart-ros <240818@usn.no>
2024-02-20 11:05:38 +0100

    rebuilding

--- build/asv_nav/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = 'C:\\Users\\edvar\\programming\\ros2_ws\\install\\asv_nav'
--- build/ros_tcp_endpoint/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = 'C:\\Users\\edvar\\programming\\ros2_ws\\install\\ros_tcp_endpoint'
